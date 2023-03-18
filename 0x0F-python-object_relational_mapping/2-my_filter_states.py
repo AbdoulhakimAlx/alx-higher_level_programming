07ee42ed015f63fc8e4704b242333a0f93efb184
Bouari abdoul hakim <bouariabdoulhakim@gmail.com>
2023-03-18 20:20:58 +0000

    Second commit

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,4 +1,6 @@
-s script takes in an argument and
+#!/usr/bin/python3
+"""
+This script takes in an argument and
 displays all values in the states
 where `name` matches the argument
 from the database `hbtn_0e_0_usa`.
